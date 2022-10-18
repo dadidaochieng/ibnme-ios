30c0f0cf29a8adc1fe856866b4927b74fd654f00
Odhiambo Ochieng <dadidaochieng@gmail.com>
2022-10-18 12:42:34 +0300

    Initial Commit

---
+++ ibnme/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ibnme
+//
+//  Created by Odhiambo Ochieng on 18/10/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ibnme/ibnmeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ibnmeApp.swift
+//  ibnme
+//
+//  Created by Odhiambo Ochieng on 18/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct ibnmeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
