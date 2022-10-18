013f747a050b5aafc8996c2211a7a24a7213c075
Odhiambo Ochieng <dadidaochieng@gmail.com>
2022-10-18 20:48:07 +0300

    WIP Welcome screen

--- ibnme/ContentView.swift
+++ ibnme/ContentView.swift
@@ -9,18 +9,48 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, IBNME!")
+        ZStack{
+            Color(.lightGray).edgesIgnoringSafeArea(.all)
+            VStack {
+                //            Image()
+                //                .imageScale(.large)
+                //                .foregroundColor(.accentColor)
+                PrimaryButton(title: "Get Started")
+                Text("Sign In")
+                    .font(.title3)
+                    .fontWeight(.bold)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color.cyan)
+                    .cornerRadius(50.0)
+                
+                HStack {
+                    Text("New around here?")
+                    Text("Sign Up")
+                        .foregroundColor(<#T##color: Color?##Color?#>)
+                }
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct PrimaryButton: View {
+    var title: String
+    var body: some View {
+        Text(title)
+            .font(.title3)
+            .fontWeight(.bold)
+            .foregroundColor(.white)
+            .frame(maxWidth:.infinity)
+            .padding()
+            .background(Color(.blue))
+            .cornerRadius(50)
     }
 }
